dadf4bad30ece4313500039d61bb148bf572fb0e
Sophia RK <130664417+s0pa-fria@users.noreply.github.com>
2023-06-17 23:38:13 -0300

    socorro, oq rolou???

--- Pop-up/popfinal.js
+++ Pop-up/popfinal.js
@@ -1,17 +1,3 @@
-// document.getElementById('fechar').addEventListener('click', function() {
-  
- 
-//     document.getElementById('popup').style.display = 'none';
-//     });
-
-function fecharpopup(){
-    // Abre uma nova janela
-    var novaJanela = window.open('', '', 'width=500,height=300');
-  
-    // Escreve o conteúdo na nova janela
-    novaJanela.document.write('<h1>Nova Janela</h1>');
-    novaJanela.document.write('<p>Esta é uma nova janela.</p>');
-  
-    // Fecha a janela original
-    window.close();
-  }
+document.getElementById('fechar').addEventListener('click', function() {
+  document.getElementById('popup').style.display = 'none';
+});
